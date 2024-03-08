eb0c0f7fe3838ab9817c03ea703a280c9be4ff31
David Wiebe <drwho.david.wiebe@gmail.com>
2024-03-07 21:22:05 -0800

    Begin read_dbf at start of file body

--- data_collection.py
+++ data_collection.py
@@ -5,7 +5,7 @@ import codecs
 # read a single DBF file
 def read_dbf(file: os.path):
     with codecs.open(file, "rb") as handle:
-        handle.seek(1000)
+        handle.seek(162)
         byte = handle.read(1000)
         print(byte)
 
@@ -13,7 +13,7 @@ def read_dbf(file: os.path):
 
 
 # read many DBF files in the same folder
-def read_dbf(folder: os.path, files: list[str]):
+def read_many_dbf(folder: os.path, files: list[str]):
     result = []
     for file in files:
         path = os.path.join(folder, file)
--- main.py
+++ main.py
@@ -6,5 +6,5 @@ import os
 if __name__ == "__main__":
     folder = os.path.join(os.pardir, "british-columbia-latest-free.shp")
     files = ["gis_osm_landuse_a_free_1.dbf"]
-    data = data_collection.read_dbf(folder, files)
+    data = data_collection.read_many_dbf(folder, files)
     pass
